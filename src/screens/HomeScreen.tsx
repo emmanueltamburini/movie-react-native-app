1bc7e6785a6cc865c1916d985a67ca0ec4357cd2
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2023-03-29 09:25:05 -0400

    movie type has added

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,10 +2,13 @@ import React, {useEffect} from 'react';
 import {View, Text} from 'react-native';
 import movieDB from '../api/movieDB';
 import {NOW_PLAYING_PATH} from '../constant/path';
+import {MovieDBNowPlaying} from '../interfaces/movieInterface';
 
 export const HomeScreen = () => {
   useEffect(() => {
-    movieDB.get(NOW_PLAYING_PATH).then(resp => console.log(resp.data));
+    movieDB
+      .get<MovieDBNowPlaying>(NOW_PLAYING_PATH)
+      .then(resp => console.log(resp.data.results[0].original_title));
   }, []);
 
   return (
