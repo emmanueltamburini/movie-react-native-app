1c32b66ba591abc71c6f433d57bd8eacc5dd7b2f
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2023-04-02 20:15:31 -0400

    context has added

--- App.tsx
+++ App.tsx
@@ -2,11 +2,22 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {Navigation} from './src/navigation/Navigation';
+import {GradientProvider} from './src/context/GradientContext';
+
+interface Props {
+  children: JSX.Element | JSX.Element[];
+}
+
+const AppState = ({children}: Props) => (
+  <GradientProvider>{children}</GradientProvider>
+);
 
 export const App = () => {
   return (
     <NavigationContainer>
-      <Navigation />
+      <AppState>
+        <Navigation />
+      </AppState>
     </NavigationContainer>
   );
 };
---
+++ src/context/GradientContext.tsx
@@ -0,0 +1,68 @@
+import React, {createContext, useState, useMemo} from 'react';
+
+interface Props {
+  children: JSX.Element | JSX.Element[];
+}
+
+interface ContextProps {
+  colors: ImageColors;
+  prevColors: ImageColors;
+  dispatchColor: (currentColors: ImageColors) => void;
+  dispatchPreviousColor: (currentColors: ImageColors) => void;
+}
+
+interface ImageColors {
+  primary: string;
+  secondary: string;
+}
+
+const providerValueDummy = {
+  colors: {
+    primary: 'transparent',
+    secondary: 'transparent',
+  },
+  prevColors: {
+    primary: 'transparent',
+    secondary: 'transparent',
+  },
+  dispatchColor: () => {},
+  dispatchPreviousColor: () => {},
+};
+
+export const GradientContext = createContext<ContextProps>({
+  ...providerValueDummy,
+});
+
+export const GradientProvider = ({children}: Props) => {
+  const [colors, setColors] = useState<ImageColors>({
+    primary: 'transparent',
+    secondary: 'transparent',
+  });
+
+  const [prevColors, setPrevColors] = useState<ImageColors>({
+    primary: 'transparent',
+    secondary: 'transparent',
+  });
+
+  const dispatchColor = (currentColors: ImageColors) =>
+    setColors(currentColors);
+
+  const dispatchPreviousColor = (currentColors: ImageColors) =>
+    setPrevColors(currentColors);
+
+  const providerValue = useMemo(
+    () => ({
+      colors,
+      prevColors,
+      dispatchColor,
+      dispatchPreviousColor,
+    }),
+    [colors, prevColors],
+  );
+
+  return (
+    <GradientContext.Provider value={providerValue}>
+      {children}
+    </GradientContext.Provider>
+  );
+};
